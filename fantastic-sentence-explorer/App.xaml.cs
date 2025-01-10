66286bb740f9eef5d667ebea0bea2fd622b549af
GreatHuang2007 <hym2007@outlook.com>
2025-01-10 22:13:17 +0800

    Optimize layout, add App name and add EditorConfig

--- fantastic-sentence-explorer/App.xaml.cs
+++ fantastic-sentence-explorer/App.xaml.cs
@@ -1,14 +1,11 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace fantastic_sentence_explorer
+﻿namespace fantastic_sentence_explorer
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        public readonly static string DisplayName = "Fantastic Sentence Explorer";
     }
 
 }
